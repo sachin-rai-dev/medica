dc3512ad4f27965a10a329b991ef42af12bb5f3d
sachin-rai-dev <sachinrai6782000@gmail.com>
2024-11-28 17:40:58 +0530

    bn

--- app/(root)/dashboard/Departments/page.jsx
+++ app/(root)/dashboard/Departments/page.jsx
@@ -147,7 +147,7 @@ function Dialogin({ data, btntitel, title, description, datafecher }) {
     }
     console.log(2)
     if (filter.length < numberofdepartment) {
-      console.log(2)
+      console.log(2.5)
       if (filter.includes(departmentname)) {
         toast({
           title: (
