7a1f04a679a696e57182d2dc66dd062df7daf139
sachin-rai-dev <sachinrai6782000@gmail.com>
2025-02-08 18:29:32 +0530

    Add App_User schema and implement POST endpoint for user data creation

--- app/api/user/appusers/route.js
+++ app/api/user/appusers/route.js
@@ -1,6 +1,7 @@
 import { conect } from '@/functions/db'
 import { User } from '@/scema/user'
 import { NextResponse } from 'next/server'
+import { App_User } from '@/scema/appuser'
 
 
 export async function GET(req) {
@@ -10,6 +11,29 @@ export async function GET(req) {
     let data = await User.find()
 
     return NextResponse.json({ data })
+
+}
+
+export async function POST(req) {
+
+    try {
+
+        let data = await req.json()
+
+        console.log(data)
+
+        await conect()
+
+        let created_data = await App_User.create(data)
+
+        console.log(created_data)
+
+        return NextResponse.json({ created_data })
+
+    } catch (error) {
+
+        return NextResponse.json({ error })
+    }
 
 }
 
