0a23aa719659b39aa0e990409cb29be043178329
sachin-rai-dev <sachinrai6782000@gmail.com>
2025-02-11 17:56:44 +0530

    Refactor App_User schema and implement GET endpoint for user data retrieval

---
+++ app/api/user/appusers/appusera/route.js
@@ -0,0 +1,29 @@
+import { conect } from '@/functions/db'
+import { App_User } from '@/scema/appuser'
+import { NextResponse } from 'next/server'
+
+export async function GET(req) {
+
+    try {
+
+        let urlParams = new URL(req.url).searchParams;
+        let email = urlParams.get('email');
+        let name = urlParams.get('name');
+        console.log(email,name)
+
+        await conect()
+
+        let created_data = await App_User.findOne({ email:email,name:name })
+
+        console.log(created_data)
+
+        return NextResponse.json({ created_data })
+
+    } catch (error) {
+
+        console.log(error)
+
+        return NextResponse.json({ error })
+    }
+
+}
